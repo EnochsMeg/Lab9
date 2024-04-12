cb8701261d837ffd7c557c99ca29a88ca65ca2dd
carolinamabad <158006775+carolinamabad@users.noreply.github.com>
2024-04-12 15:20:58 -0400

    Uploaded decoder

---
+++ decoder.py
@@ -0,0 +1,10 @@
+###carolinmabad decoder for megan enochs
+
+def decoder(string):
+    password = ''
+    decode1 = 0
+    for num in string:
+        decode1 = int(num)-3
+        password += string(decode1)
+        decode1 = 0
+    return password
